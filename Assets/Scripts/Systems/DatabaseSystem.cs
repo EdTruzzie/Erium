c155e482ab2a358ae25afa5e785e7044e8f2e8de
Truzzie <edward.truscott@yahoo.com>
2026-09-05 18:02:11 +1000

    Add DatabaseSystem class for database management

---
+++ Assets/Scripts/Systems/DatabaseSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace Erium 
+{
+	public class DatabaseSystem : MonoBehaviour
+	{
+		void Start()
+		{
+			
+		}
+		void Update()
+		{
+			
+		}
+	}
+}
